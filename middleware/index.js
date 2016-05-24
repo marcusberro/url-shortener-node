7a6cb706fa7a0bbc702a204d6f9e6ffa2f214854
Marcus Berro <marcusberro@gmail.com>
2016-05-24 00:54:46 -0300

    Adding common handlers

--- middleware/index.js
+++ middleware/index.js
@@ -1,1 +1,9 @@
-module.exports = (server) => {}
+'use strict';
+
+var restify = require('restify');
+
+module.exports = (server) => {
+  server.use(restify.queryParser());
+  server.use(restify.bodyParser());
+  server.pre(restify.pre.sanitizePath());
+}
